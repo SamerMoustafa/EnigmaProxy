8629c64b85311887417c3b864566a84c7a01dfbd
Samer Moustafa <dev.samer@hotmail.com>
2017-08-21 23:38:32 +0200

    Basic Library Setup

---
+++ EnigmaLibrary/Request.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnigmaLibrary
+{
+    class Request
+    {
+
+        public String Path;
+
+        public Dictionary<String, String> Parameters = new Dictionary<string, string>();
+
+        public String AccessToken;
+
+    }
+}
