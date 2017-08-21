5c03285ca4f555a65482f8d4ea00891a85e0fc57
Samer Moustafa <dev.samer@hotmail.com>
2017-08-22 00:23:55 +0200

    Basic Server Class Structure

---
+++ EnigmaServer/Server.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnigmaServer
+{
+    class Server
+    {
+
+        protected static Server _Instance = new Server();
+
+        protected Socket _ServerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        protected const int MAX_BUFFER_SIZE = 2048;
+
+        protected const int PORT = 90;
+
+        protected Server()
+        {
+
+        }
+
+        public static Server GetInstance()
+        {
+            return _Instance;
+        }
+
+        public void Setup()
+        {
+
+        }
+    }
+}
